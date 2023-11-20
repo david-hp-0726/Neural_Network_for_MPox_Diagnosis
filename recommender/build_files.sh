2eba63845254b0a7b6c5ef61de3c3142cde40d51
david-hp-0726 <xche653@emory.edu>
2023-11-20 15:31:14 -0500

    configure project for vercel

---
+++ recommender/build_files.sh
@@ -0,0 +1,4 @@
+# build_files.sh
+pip install tensorflow
+pip install numpy
+python3.9 manage.py collectstatic
--- recommender/recommender/settings.py
+++ recommender/recommender/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ah7a3wox%iprw+hele7z2=7%&2b(hpf1=kyvrk2%p--sjk9h%^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
